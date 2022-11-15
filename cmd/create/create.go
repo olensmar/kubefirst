f43fe793aaa5d546f413c3e7ed4feabb6629250d
Jared Edwards <jared@kubefirst.com>
2022-11-14 18:53:38 -0700

    initial validation - next step

--- cmd/create/create.go
+++ cmd/create/create.go
@@ -3,8 +3,12 @@ package create
 import (
 	"errors"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/kubefirst/kubefirst/pkg"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -40,9 +44,46 @@ func NewCommand() *cobra.Command {
 	return createCmd
 }
 
+type CivoExecutionControl struct {
+	Step1 string
+	Step2 string
+}
+
 func runCreate(cmd *cobra.Command, args []string) error {
 
-	fmt.Println("run new create Cmd")
+	executionControl := CivoExecutionControl{}
+	executionControl.Step1 = "kubefirst.init.env-check.complete"
 
-	return errors.New("fake error")
+	//* 1. civo token - validate and require
+	//* 2. github token - validate and require
+	//*
+	//*
+
+	silentMode := false
+
+	if !viper.GetBool(executionControl.Step1) {
+		pkg.InformUser("validating `kubefirst init` environment", silentMode)
+		log.Println("INIT: checking environment variables")
+
+		civoToken := os.Getenv("CIVO_TOKEN") //* todo fix GITHUB_AUTH_TOKEN
+		if civoToken == "" {
+			log.Println("Unauthorized: No CIVO_TOKEN environment variable is present.")
+			return fmt.Errorf("unauthorized: missing CIVO_TOKEN environment variable")
+		}
+		log.Println("INIT: CIVO_TOKEN is set")
+
+		// todo add the ephemeral token logic here
+		githubToken := os.Getenv("GITHUB_TOKEN")
+		if githubToken == "" {
+			log.Println("Unauthorized: No GITHUB_TOKEN environment variable is present.")
+			return fmt.Errorf("unauthorized: missing GITHUB_TOKEN environment variable")
+		}
+		log.Println("INIT: GITHUB_TOKEN is set")
+
+	} else {
+		log.Println("INIT: environment variables all set - continuing")
+		log.Println("executionControl.Step1 complete")
+	}
+
+	return errors.New("\n\nno error: get the next rock")
 }
