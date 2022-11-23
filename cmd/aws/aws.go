384996f3a7108f18ecb2d1c5276c944f5cb9ce40
Jared Edwards <jared@kubefirst.com>
2022-11-22 18:32:30 -0700

    initial aws command

---
+++ cmd/aws/aws.go
@@ -0,0 +1,48 @@
+package local
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	// enableConsole bool
+	gitProvider string
+)
+
+func NewCommand() *cobra.Command {
+
+	awsCmd := &cobra.Command{
+		Use:     "aws",
+		Short:   "kubefirst aws installation",
+		Long:    "kubefirst aws",
+		PreRunE: validateAws,
+		RunE:    runAws,
+		// PostRunE: runPostAws,
+	}
+
+	// awsCmd.Flags().BoolVar(&enableConsole, "enable-console", true, "If hand-off screen will be presented on a browser UI")
+	awsCmd.Flags().StringVar(&gitProvider, "git-provider", "github", "the git provider to use. (i.e. gitlab|github)")
+
+	// on error, doesnt show helper/usage
+	awsCmd.SilenceUsage = true
+
+	// wire up new commands
+	// awsCmd.AddCommand(NewCommandConnect())
+
+	return awsCmd
+}
+
+func runAws(cmd *cobra.Command, args []string) error {
+
+	// config := configs.ReadConfig()
+	gitProviderFlag, err := cmd.Flags().GetString("git-provider")
+	if err != nil {
+		return err
+	}
+	log.Println("this is a test", gitProviderFlag)
+
+	return nil
+
+}
---
+++ cmd/aws/prerun.go
@@ -0,0 +1,12 @@
+package local
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func validateAws(cmd *cobra.Command, args []string) error {
+	log.Println("testing validateAws: todo implement")
+	return nil
+}
